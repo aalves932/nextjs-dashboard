1e7d0041bd906f4c3ab6b1bb58858d74b438d60a
Andre Alves <andre.alves@sixsigma.com.br>
2025-06-17 14:34:10 -0300

    feat: Refactor dashboard components to use Suspense for loading states and improve data fetching

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,12 +16,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -44,6 +44,7 @@ export async function fetchLatestInvoices() {
       amount: formatCurrency(invoice.amount),
     }));
     // console.log('Latest invoices fetched:', latestInvoices);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
