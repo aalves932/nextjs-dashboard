4af9c66c9518d79c90ff6f895cc14606c4b60686
Andre Alves <andre.alves@sixsigma.com.br>
2025-06-17 12:38:05 -0300

    feat: Enhance dashboard with revenue chart and latest invoices components

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,11 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
+            <strong>Bem-vindo ao Acme.</strong> Este é um exemplo do{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              Curso Aprenda Next.js
             </a>
-            , brought to you by Vercel.
+            , oferecido à você pela Vercel.
           </p>
           <Link
             href="/login"
