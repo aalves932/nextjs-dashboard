9d7f0ec6d157af79f558b2315989c097519f6321
Andre Alves <andre.alves@sixsigma.com.br>
2025-07-17 14:34:01 -0300

    refactor: update card types and remove unused variables for improved clarity

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,56 +1,39 @@
 import {
   BanknotesIcon,
+  CircleStackIcon,
   ClockIcon,
-  UserGroupIcon,
-  InboxIcon,
+  CurrencyDollarIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import { getSigmaDataStats } from '@/app/lib/data';
 import { formatCurrency } from "@/app/lib/utils";
 
-
 const iconMap = {
-  collected: BanknotesIcon,
-  customers: UserGroupIcon,
-  pending: ClockIcon,
-  invoices: InboxIcon,
+  bankNotes: BanknotesIcon,
+  coins: CircleStackIcon,
+  ytdIcon: ClockIcon,
+  currency: CurrencyDollarIcon,
 };
 
 export default async function CardWrapper() {
-  
-  // const {
-  //   numberOfInvoices,
-  //   numberOfCustomers,
-  //   // totalPaidInvoices,
-  //   totalPendingInvoices,
-  // } = await fetchCardData();
 
   const { total_amount, total_last_12_months, total_ytd, total_current_month } = await getSigmaDataStats();
 
   return (
     <>
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-      <Card title="Faturamento Total" value={formatCurrency(total_amount*100)} type="collected"/>
-      <Card title="Faturamento 12 Meses" value={formatCurrency(total_last_12_months*100)} type="pending" />
-      <Card title="Faturamento YTD" value={formatCurrency(total_ytd*100)} type="invoices" />
-      <Card title="Faturamento do Mês" value={formatCurrency(total_current_month*100)} type="customers"/>
+      <Card title="Faturamento Total" value={formatCurrency(total_amount*100)} type="bankNotes"/>
+      <Card title="Faturamento 12 Meses" value={formatCurrency(total_last_12_months*100)} type="coins" />
+      <Card title="Faturamento YTD" value={formatCurrency(total_ytd*100)} type="ytdIcon" />
+      <Card title="Faturamento do Mês" value={formatCurrency(total_current_month*100)} type="currency"/>
     </>
   );
 }
 
-export function Card({
-  title,
-  value,
-  type,
-}: {
-  title: string;
-  value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
-}) {
+export function Card({ title, value, type,}: {  title: string; value: number | string; type: 'ytdIcon' | 'currency' | 'coins' | 'bankNotes';}) {
+
   const Icon = iconMap[type];
 
   return (
-    // font-medium border-2 border-black rounded-xl px-3 py-2 bg-brand hover:bg-yellow-300 hard-shadow-sm
     <div className="rounded-xl bg-yellow-400 border-2 border-black p-2 shadow-sm shadow-black">
       <div className="flex p-2 items-center">
         {Icon ? <Icon className="h-6 w-6 text-gray-700" /> : null}
